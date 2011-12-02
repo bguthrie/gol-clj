06870ad12812ec78354ed5f416b05802943f74e9
Brian Guthrie <btguthrie@gmail.com>
2011-12-02 16:57:46 +1100

    Basic ability to count all neighbours of a cell.

--- test/gol_clj/test/core.clj
+++ test/gol_clj/test/core.clj
@@ -2,7 +2,7 @@
   (:use [gol-clj.core])
   (:use [clojure.test]))
 
-(deftest cell-alive-for-live-cell
+(deftest cell-alive-for-live-cell-should-use-rule
   (is (false? (cell-alive? { :live true :neighbours 0 })))
   (is (false? (cell-alive? { :live true :neighbours 1 })))
   (is (true?  (cell-alive? { :live true :neighbours 2 })))
@@ -11,6 +11,33 @@
   (is (false? (cell-alive? { :live true :neighbours 8 })))
   )
 
-(deftest cell-alive-for-dead-cell
-  (is (false? (cell-alive? { :live false :neighbours 2 }))))
-  ; (is (true? (cell-alive? { :live false :neighbours 3 }))))
+(deftest cell-alive-for-dead-cell-should-use-rule
+  (is (false? (cell-alive? { :live false :neighbours 2 })))
+  (is (true?  (cell-alive? { :live false :neighbours 3 })))
+  )
+
+(deftest all-neigbhours-one-row
+  (is (= 0 (all-neighbours 0 0 [[0]       [] []])))
+  (is (= 1 (all-neighbours 0 0 [[0 1]     [] []])))
+  (is (= 2 (all-neighbours 0 1 [[1 1 1]   [] []])))
+  (is (= 2 (all-neighbours 0 1 [[1 0 1]   [] []])))
+  (is (= 2 (all-neighbours 0 1 [[1 0 1 1] [] []])))
+  )
+
+(deftest row-neighbours-should-return-subvec
+  (is (= []      (row-neighbours []          0)))
+  (is (= [0]     (row-neighbours [0]         0)))
+  (is (= [1 0]   (row-neighbours [0 1 0]     2)))
+  (is (= [1 1 1] (row-neighbours [0 1 1 1 0] 2)))
+  (is (= [1 0]   (row-neighbours [1 0 1 1]   0)))
+  )
+
+; (deftest gol-step-visual-test
+;   (is (= 
+;         [[0 0 0]
+;          [0 0 0]
+;          [0 0 0]]
+;       (gol-step 
+;         [[0 0 0]
+;          [0 1 0]
+;          [0 0 0]]))))
