59d89c7a93b80832f1a1dd14ad385d7ae0c4566e
Brian Guthrie <btguthrie@gmail.com>
2012-01-18 13:45:36 -0800

    Basic README and description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject gol-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "bguthrie's implementation of Conway's Game of Life in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[speclj "1.2.0"]]
   :test-path "spec/")
